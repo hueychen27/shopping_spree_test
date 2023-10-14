e4652235514a773d0403c3bd5d36de852afe079c
Da_Person <hueychen27@outlook.com>
2023-10-14 09:16:04 -0700

    Add subtotal functionality and limit quantity

--- script.js
+++ script.js
@@ -53,10 +53,10 @@ function createItemBox() {
         <h1>${name2}</h1>
         <p>Description:</p>
         <ul>
-            <li>Name: <input class="nameThing" type="text" data-lastValid="${name}" value="${name}" oninput="if (items[\`\${ this.value }\`] == undefined && new RegExp(/^[\\w\\-\\' ]+$/).test(this.value)) {items[\`\${ this.value }\`] = items[\`\${ this.getAttribute('data-lastValid') }\`],delete items[\`\${ this.getAttribute('data-lastValid') }\`],this.setAttribute('data-lastValid', this.value)} else {this.value = this.getAttribute('data-lastValid')}"></li>
-            <li>Price: $<input class="priceThing" type="text" data-lastValid="1.00" oninput="if (new RegExp(/^[0-9]+\\.?([0-9]{1,2})?$/).test(this.value) || this.value == '') {this.setAttribute('data-lastValid', this.value), items[\`\${ this.parentElement.previousElementSibling.querySelector('input').getAttribute('data-lastValid') }\`].price = this.value} else { this.value = this.getAttribute('data-lastValid')} // https://stackoverflow.com/a/41981763/15055490" value="1.00"></li>
+            <li>Name: <input class="nameThing" type="text" data-lastValid="${name}" value="${name}" oninput="if (items[\`\${ this.value }\`] == undefined && new RegExp(/^[\\w\\-\\' ]+$/).test(this.value)) {items[\`\${this.value}\`] = items[\`\${ this.getAttribute('data-lastValid')}\`],delete items[\`\${ this.getAttribute('data-lastValid')}\`],this.setAttribute('data-lastValid', this.value)} else {this.value = this.getAttribute('data-lastValid')}"></li>
+            <li>Price: $<input class="priceThing" type="text" data-lastValid="1.00" oninput="if (new RegExp(/^[0-9]+\\.?([0-9]{1,2})?$/).test(this.value) || this.value == '') {this.setAttribute('data-lastValid', this.value), items[\`\${ this.parentElement.previousElementSibling.querySelector('input').getAttribute('data-lastValid')}\`].price = this.value} else { this.value = this.getAttribute('data-lastValid')} // https://stackoverflow.com/a/41981763/15055490" value="1.00"></li>
         </ul>
-        <label>Quantity: <input class="quantityThing" type="text" data-lastValid="1" oninput="if (new RegExp(/^[0-9]+$/).test(this.value)) {this.setAttribute('data-lastValid', this.value), items[\`\${ this.parentElement.previousElementSibling.querySelector('input').getAttribute('data-lastValid') } \`].quantity = this.value} else { this.value = this.getAttribute('data-lastValid')} // https://stackoverflow.com/a/41981763/15055490"></label>
+        <label>Quantity: <input class="quantityThing" type="text" data-lastValid="1" oninput="if (new RegExp(/^[0-9]{1,10}$/).test(this.value)) {this.setAttribute('data-lastValid', this.value), items[\`\${this.parentElement.previousElementSibling.querySelector('input').getAttribute('data-lastValid')}\`].quantity = this.value} else { this.value = this.getAttribute('data-lastValid')} // https://stackoverflow.com/a/41981763/15055490"></label>
         <button class="remove" onclick="removeItemBox(this.parentElement);"><i class="fas fa-trash-xmark fa-xl"></i>Delete</button>
     `;
     items[name] = {
@@ -75,3 +75,15 @@ function removeItemBox(element) {
     delete items[`${element.querySelector(".nameThing").getAttribute("data-lastValid")}`];
     element.remove();
 }
+
+function numberWithCommas(x) {
+    return x < 1000000 ? x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") : numberformat.format(x);
+}
+
+function updateTotal() {
+    const table = document.getElementById("table");
+    table.querySelector("#subtotal").innerHTML = "$" + numberWithCommas((Object.values(items).reduce((a, b) => a + b.price * b.quantity, 0)).toFixed(2));
+    window.requestAnimationFrame(updateTotal);
+}
+
+window.requestAnimationFrame(updateTotal)
